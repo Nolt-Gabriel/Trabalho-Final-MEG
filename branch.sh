0cb587bf8307d323eaa4d7e3dbe7a03be87f2bba
Nicholas Gabriel Silva Soares <nicholas.soares@escolar.ifrn.edu.br>
2026-01-20 17:07:01 -0300

    feat: Script de Branchs

--- branch.sh
+++ branch.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#Esse Script atualiza as branchs locais, trazendo as branchs remotas
+#Utilize quando você não estiver vendo as branchs remotas no local
+#Para usar executar, digite no terminal: chmod -x ./branch.sh , para tornar o arquivo executável
+#Depois basta digitar ./branch.sh no terminal que ele executará o Script
+
+
 git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
 git fetch --all
 git pull --all
