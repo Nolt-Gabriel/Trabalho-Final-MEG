928e0dc0ee6350fb046748ece40b49fae6e9835e
Nicholas Gabriel Silva Soares <nicholas.g@academico.ifrn.edu.br>
2026-01-21 11:44:12 -0300

    feat: Script de Branch - by Nolt

--- branch.sh
+++ branch.sh
@@ -5,7 +5,22 @@
 #Para usar executar, digite no terminal: chmod -x ./branch.sh , para tornar o arquivo executável
 #Depois basta digitar ./branch.sh no terminal que ele executará o Script
 
+echo "<<<<< Script de Branchs >>>>>"
+echo "<<<<<    By Noltuser    >>>>>"   
+echo ""
+echo ">Esse Script vai atualizar as branchs locais e sincronizar"
+echo ">com as branchs remotas"
+echo ""
+read -p "Deseja continuar? (s/n): " resposta
 
-git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
-git fetch --all
-git pull --all
+if [[ $resposta == "s" || $resposta == "S" ]]; then
+    echo "Continuando o script..."
+    git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
+    git fetch --all
+    git pull --all
+
+else
+    echo "Script interrompido."
+    exit 0
+fi
+
