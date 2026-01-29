56fa89c5bf9efd10d1057ca29ed5add4a40942c7
Nicholas Gabriel Silva Soares <nicholas.soares@escolar.ifrn.edu.br>
2026-01-29 15:19:05 -0300

    feat: Base da tabela

--- models.py
+++ models.py
@@ -5,19 +5,7 @@ class Usuario(Base):
 
     __tablename__ = "usuarios"
 
-    id = Column(Integer, primary_key=True)
-    nome = Column(String)
-    nascimento = Column(String)
-    cpf = Column(String)
-    telefone = Column(String)
-    email = Column(String, unique=True)
-    senha = Column(String)
-    cep = Column(String)
-    rua = Column(String)
-    bairro = Column(String)
-    casa = Column(String)
-    deficiencia = Column(String)
-    genero = Column(String)
+   
 
     
     
